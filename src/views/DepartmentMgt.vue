6d093abe52135a3afc56120194fcd1cc79a70a3c
bxr <1234567>
2023-04-12 16:01:56 +0800

    bxr写了科室管理和缴费页面-未写完

---
+++ src/views/DepartmentMgt.vue
@@ -0,0 +1,157 @@
+<template>
+    <div class="box1">
+        <el-button type="primary" plain>增加</el-button>
+        <el-tabs :tab-position="tabPosition"  class="demo-tabs" @tab-click="handleClick">
+        <el-tab-pane v-for="item in list " :key="item.id" :label= item.dename >
+<!--             <el-table :data="tableData" border style="width: 100%" v-for="date in list2 " :key="date.id" :label= date.dename>
+                  <el-table-column prop="name" label="子级科室id" width="160" />
+                  <el-table-column prop="date" label="子级科室名" width="160" />
+                  <el-table-column prop="name" label="负责人" width="160" />
+                  <el-table-column prop="name" label="负责人联系电话" width="160" />
+                  <el-table-column prop="name" label="科室状态" width="160" />
+                  <el-table-column prop="name" label="父级科室id" width="160" />
+            </el-table> -->
+        </el-tab-pane>
+    </el-tabs>
+    </div>
+</template>
+
+<script setup>
+import { timePanelSharedProps } from 'element-plus/es/components/time-picker/src/props/shared';
+import { ref,computed } from 'vue'
+const tabPosition = ref('left');
+/* const activeName = computed(() => list.value[0].dename) */
+const handleClick = () =>{
+
+}
+let list = [
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+]
+let list2 = [
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+    { id: 1, dename: '消暑' },
+    { id: 2, dename: '小米' },
+    { id: 3, dename: '小麦' },
+]
+const tableData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+
+]
+
+
+</script>
+
+<style scoped>
+.box1{
+    margin: 150px 100px;
+    width: 700px;
+    height: 400px;
+}
+.box1 .el-button{
+    margin-left: 480px;
+    margin-bottom: 10px;
+}
+.demo-tabs {
+
+    height: 400px;
+    background-color: #d6cbfa;
+    opacity: .7;
+    font-size: 30px;
+}
+/* .el-tab-pane {
+    background-color: #d6cbfa;
+    opacity: .7;
+    width: 200px;
+    height: 400px;
+} */
+
+/* .el-main {
+    background-color: #e1d8fe;
+    opacity: .7;
+    height: 400px;
+} */
+
+/* .common-layout .office {
+    border-bottom: 1px solid black;
+    font-size: 30px;
+    height: 50px;
+    line-height: 50px;
+}
+ */
+/*.common-layout .el-main .el-table{
+     border-top:1px solid grey;
+    border-right:1px solid grey; 
+    
+}*/
+/* .common-layout .el-main .el-table .el-table-column{
+    border: 1px solid grey;
+} */
+/* .common-layout .el-main .el-table .el-pagination{
+    margin-bottom: 3px;
+} */
+</style>
---
+++ src/views/Pay.vue
@@ -0,0 +1,204 @@
+<template>
+    <div class="pay">
+        <h4>门诊收退记录</h4>
+        <div class="box1">
+            <el-select v-model="value" class="m-2" placeholder="Select">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
+            <el-select v-model="value" class="m-2" placeholder="Select">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
+            <el-select v-model="value" class="m-2" placeholder="Select">
+                <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value" />
+            </el-select>
+            <el-input v-model="form.name" value="输入单据号" />
+            <el-input v-model="form.name" value="输入患者姓名" />
+            <div class="demo-date-picker">
+                <div class="block">
+                    <el-date-picker v-model="value1" type="daterange" range-separator="To" start-placeholder="Start date"
+                        end-placeholder="End date" :size="size" />
+                </div>
+            </div>
+        </div>
+        <div class="box2">
+            <el-button type="query" plain>查询</el-button>
+            <el-button type="anew" plain>条件重置</el-button>
+            <el-button type="print" plain>打印</el-button>
+            <el-button type="success" plain>Success</el-button>
+        </div>
+        <div class="box3">
+            <el-table :data="tableData" border style="width: 100%">
+                <el-table-column prop="date" label="Date" width="180" />
+                <el-table-column prop="name" label="Name" width="180" />
+                <el-table-column prop="address" label="Address" />
+            </el-table>
+            <div>
+                <el-pagination small layout="prev, pager, next" :total="50" />
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import { ref } from 'vue';
+// do not use same name with ref
+//el-form的逻辑
+const form = reactive({
+    name: '',
+    region: '',
+    date1: '',
+    date2: '',
+    delivery: false,
+    type: [],
+    resource: '',
+    desc: '',
+})
+const value = ref('')
+
+const options = [
+    {
+        value: 'Option1',
+        label: 'Option1',
+    },
+    {
+        value: 'Option2',
+        label: 'Option2',
+    },
+    {
+        value: 'Option3',
+        label: 'Option3',
+    },
+    {
+        value: 'Option4',
+        label: 'Option4',
+    },
+    {
+        value: 'Option5',
+        label: 'Option5',
+    },
+]
+
+
+//表格
+const tableData = [
+    {
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+    {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles',
+    },
+]
+const onSubmit = () => {
+    console.log('submit!')
+}
+//
+const size = ref<'default' | 'large' | 'small'>('default')
+
+const value1 = ref('')
+const value2 = ref('')
+
+const shortcuts = [
+    {
+        text: 'Last week',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+            return [start, end]
+        },
+    },
+    {
+        text: 'Last month',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+            return [start, end]
+        },
+    },
+    {
+        text: 'Last 3 months',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+            return [start, end]
+        },
+    },
+]
+</script>
+  
+
+<style scoped>
+.pay{
+    margin: 100px ;
+}
+.box1 {
+    width: 100%;
+    height: 60px;
+    background: pink;
+}
+
+.el-select {
+    width: 100px;
+    margin-left: 20px;
+    margin-top: 3px;
+}
+
+.el-input {
+    width: 120px;
+    margin-left: 20px;
+    margin-top: 3px;
+}
+
+/* 日期 */
+.demo-date-picker {
+    display: flex;
+    width: 100%;
+    padding: 0;
+    flex-wrap: wrap;
+    display: inline;
+}
+
+.demo-date-picker .block {
+      text-align: center; 
+    border-right: solid 1px var(--el-border-color);
+    display: inline;
+    margin-left: 20px;
+    float: right;
+}
+
+.demo-date-picker .block:last-child {
+    border-right: none;
+    display: inline;
+    margin-top: 3px;
+
+}
+
+.demo-date-picker .demonstration {
+    display: inline;
+    color: var(--el-text-color-secondary);
+    font-size: 14px;
+    margin-top: 3px;
+}
+
+.box2{
+    float: right;
+    height: 40px;
+}
+</style>
